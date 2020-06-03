f89dedce7fa28767a74b23b6194ed730eb888e17
kumarv987 <vijit.kr12@gmail.com>
2020-06-04 06:03:00 +1000

    Designed login page and wrote code for UniLinkGUI starter

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class LoginPageController {
+    @FXML private Label unilinkWelcomeLabel;
+    @FXML private Button userNameEnterButton;
+    @FXML private Button exitButton;
+    @FXML private TextField userNameTextField;
+
 
 }
--- src/main/UniLinkGUI.java
+++ src/main/UniLinkGUI.java
@@ -1,6 +1,34 @@
 package main;
 
-public class UniLinkGUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.stage.*;
+import model.*;
+import controller.*;
 
+import java.io.IOException;
 
+public class UniLinkGUI extends Application {
+    public static void main(String[] args){
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        loginPageEntry(stage);
+    }
+
+    private void loginPageEntry(Stage stage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setTitle("Login");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
