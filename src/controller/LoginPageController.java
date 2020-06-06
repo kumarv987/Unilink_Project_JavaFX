89f9d9636e225fc4bb613c505f92ffae07c62eeb
kumarv987 <vijit.kr12@gmail.com>
2020-06-06 23:56:28 +1000

    Change to MainWindow from Login window is working

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -1,14 +1,28 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import model.Service;
+import model.User;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginPageController implements Initializable {
+
+    //Variables for model package
+    private Service modelServiceHandler = new Service();
+
+    // Variables from the Login window
     @FXML private Button enterButton;
     @FXML private Button exitButton;
     @FXML private TextField userNameTextField;
@@ -18,15 +32,33 @@ public class LoginPageController implements Initializable {
      * This method first checks if the anything is entered in the userNameTextField, then save the userName info, and
      * finally loads the next screen.
      ******************************************************************************************************************/
-    public void enterButtonPushed() {
-        //If the nothing is entered in the userName text field
+    public void enterButtonPushed(ActionEvent event) throws IOException, NullPointerException {
+        //If the nothing is entered in the userName text field then showcase the warning
         if(userNameTextField.getText().equalsIgnoreCase("")){
         String messageLabelWarning = "You must enter a Username first!!\n";
             this.userNameWarningLabel.setText(messageLabelWarning);
+        } else {
+            //Create new instance for user and add it to the listOfUsers
+            User newUser = new User(userNameTextField.getText());
+            modelServiceHandler.addUsersToListOfUsers(newUser);
+
+            //Setting a new stage
+            changeScreen(event);
         }
+    }
 
+    /*******************************************************************************************************************
+     * This method changes the screen when Submit button is pressed with userName entered
+     ******************************************************************************************************************/
+    private void changeScreen(ActionEvent event) throws IOException{
+        //Now loading the new stage
+        Parent mainWindowParent = FXMLLoader.load(getClass().getResource("/view/MainPage.fxml"));
+        Scene mainWindowScene = new Scene(mainWindowParent);
 
-
+        //This line get the stage information
+        Stage mainWindowStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        mainWindowStage.setScene(mainWindowScene);
+        mainWindowStage.show();
     }
 
     @Override
--- src/model/Service.java
+++ src/model/Service.java
@@ -3,6 +3,22 @@ package model;
 import java.util.ArrayList;
 
 public class Service {
-    private ArrayList<User> listOfUsers;
+    private ArrayList<User> listOfUsers = new ArrayList<>();
+
+    public ArrayList<User> getListOfUsers() {
+        return listOfUsers;
+    }
+
+    public void setListOfUsers(ArrayList<User> listOfUsers) {
+        this.listOfUsers = listOfUsers;
+    }
+
+    /*******************************************************************************************************************
+     * This method adds the user to a listOfUsers
+     ******************************************************************************************************************/
+    public void addUsersToListOfUsers(User user) {
+        listOfUsers.add(user);
+    }
+
 
 }
